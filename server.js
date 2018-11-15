f42b92f202c23ecc62549864cce4db8879d28e20
eloy ortega <info@artbyeloi.com>
2018-11-14 21:22:32 -0500

    404

--- server.js
+++ server.js
@@ -5,4 +5,6 @@ const search = require('./controllers/google.js');
 
 app.get('/search/:term', search);
 
+app.use((req, res, next) => res.send(`I'm sorry Dave. I can't do that.`));
+
 app.listen(PORT, () => console.log(`listening on port ${PORT}`));
